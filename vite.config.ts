17e03aab7b716e7a7d28244c7ca4142cd616c768
Tirado Delgado Anibal Emilio <124332136+AnibalITQ@users.noreply.github.com>
2025-02-02 00:51:29 -0600

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -21,27 +21,27 @@ export default defineConfig({
         theme_color: '#E5700F',
         icons: [
           {
-            src: '/icons/icon-72x72.png',
+            src: '/icons/logo.png',
             sizes: '72x72',
             type: 'image/png'
           },
           {
-            src: '/icons/icon-96x96.png',
+            src: '/icons/logo.png',
             sizes: '96x96',
             type: 'image/png'
           },
           {
-            src: '/icons/icon-128x128.png',
+            src: '/icons/logo.png',
             sizes: '128x128',
             type: 'image/png'
           },
           {
-            src: '/icons/icon-192x192.png',
+            src: '/icons/logo.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icons/icon-512x512.png',
+            src: '/icons/logo.png',
             sizes: '512x512',
             type: 'image/png'
           }
